60baf406edb2d62065cbca23eed42359e4a54fe5
Fabricio Miranda <fbrnery@gmail.com>
2021-03-10 19:38:51 -0300

    Add páginas de apoio/rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root "welcome#index"
   
   get 'welcome/index'
-
+  get 'welcome/historia'
+  get 'welcome/acai'
+  get 'welcome/cardapio'
+  get 'welcome/contatos'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
